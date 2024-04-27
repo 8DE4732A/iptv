6b5e8cf914e99e8687c5bdd3dc296152f19b2354
8DE4732A <liuping@liuping.win>
2024-04-27 08:33:01 +0800

    Update iptv_proxy.py

--- iptv_proxy.py
+++ iptv_proxy.py
@@ -1,74 +1,61 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import urllib.request as urllib2
-import logging
+import requests
+import httpx
 import re
-import gzip
+import base64
+from sanic import Sanic
+import sanic.log
+from sanic.log import logger
+from sanic.request import Request
+from sanic.response import json, text
 
-logging.basicConfig()
-logger = logging.getLogger(__name__)
+config = sanic.log.LOGGING_CONFIG_DEFAULTS
+config['handlers']['file'] = {
+    "class" : "logging.handlers.RotatingFileHandler",
+    "formatter": "generic",
+    "filename": "iptv.log",
+    "maxBytes": 1024000,
+    "backupCount": 10
+}
+config['handlers']['file_access'] = {
+    "class" : "logging.handlers.RotatingFileHandler",
+    "formatter": "access",
+    "filename": "iptv.log",
+    "maxBytes": 1024000,
+    "backupCount": 10
+}
+config['loggers']['sanic.root']['handlers'].append('file')
+config['loggers']['sanic.access']['handlers'].append('file_access')
+config['loggers']['sanic.error']['handlers'].append('file')
+config['loggers']['sanic.server']['handlers'].append('file')
+app = Sanic("iptv", log_config=config)
+client = httpx.AsyncClient(timeout = httpx.Timeout(5.0, connect=2.0))
 
-class ProxyHandler(BaseHTTPRequestHandler):
-    def match(self, url):
-        address = 'http://' + str(self.server.server_address[0]) + ":" + str(self.server.server_address[1]) + '/'
-        m = re.match(r'(https?:\/\/)([a-z0-9\.\:]+)(.+\.php)', url)
-        if m:
-            return url.replace(m.group(0), address + m.group(0))
-        else:
-            return url
-    def proxy(self):
-        print('请求:', self.path)
-        print('请求头:', self.headers)
-        print('请求方法:', self.command)
-        if self.command == 'POST':
-            req = self.rfile.read(int(self.headers['content-length']))
-            print('请求体:', req.decode('utf-8'))
-        else:
-            req = None
-        if self.path.endswith('.m3u'):
-            request = urllib2.Request(url='https://mirror.ghproxy.com/https://raw.githubusercontent.com/Kimentanm/aptv/master/m3u/iptv.m3u', method=self.command, data=req)
-            with urllib2.urlopen(request) as f:
-                print(f.status, f.headers)
-                self.send_response(f.status)
-                for (key, value) in f.headers.items():
-                    if key == 'Content-Type':
-                        self.send_header(key, value)
-                self.end_headers()
-                data = f.read()
-                if f.headers['Content-Encoding'] == 'gzip':
-                    data = gzip.decompress(data)
-                data = data.decode('utf-8')
-                data = '\n'.join([self.match(x) for x in data.split('\n')])
-                # data = data.decode('utf-8').replace('http://live.aptvapp.com', 'http://192.168.3.32:6666')
-                self.wfile.write(data.encode('utf-8'))
-        elif self.path.startswith('/http'):
-            header = {}
-            header['User-Agent'] = 'AptvPlayer/1.1.4'
-            request = urllib2.Request(self.path[1:], headers=header, method=self.command, data=req)
-            with urllib2.urlopen(request) as f:
-                print(f.status, f.headers)
-                self.send_response(f.status)
-                for (key, value) in f.headers.items():
-                    if key == 'Content-Type':
-                        self.send_header(key, value)
-                self.end_headers()
-                data = f.read()
-                if f.headers['Content-Encoding'] == 'gzip':
-                    data = gzip.decompress(data)
-                print(data.decode('utf-8'))
-                self.wfile.write(data)
+def match(url):
+    address = 'http://192.168.3.37:6666/proxy/'
+    m = re.match(r'(https?:\/\/)([a-z0-9\.\:]+)(.+\.php)', url)
+    if m:
+        return address + base64.urlsafe_b64encode(url.encode()).decode() + ".m3u8"
+    else:
+        return url
 
-    def do_GET(self):
-        self.proxy()
+@app.get(r'/<m3u:[a-z]+.m3u>')
+async def iptv(request, m3u):
+    if m3u == "iptv.m3u":
+        r = await client.get('https://mirror.ghproxy.com/https://raw.githubusercontent.com/Kimentanm/aptv/master/m3u/iptv.m3u')
+        html = r.text
+        html = '\n'.join([match(x) for x in html.split('\n')])
+        return text(html)
+    return text("")
 
-
-    def do_POST(self):
-        self.proxy()
-
-
-def run():
-    server_address = ('127.0.0.1', 6666)
-    httpd = HTTPServer(server_address, ProxyHandler)
-    httpd.serve_forever()
+@app.get('/proxy/<url>')
+async def proxy(request: Request, url):
+    logger.info('proxy: ' + url)
+    if url:
+        headers={"User-Agent": "AptvPlayer/1.1.4"}
+        r = await client.get(base64.urlsafe_b64decode(url.replace(".m3u8","")).decode(), headers=headers)
+        html = r.text
+        logger.info('return: ' + html)
+        return text(html)
 
 if __name__ == '__main__':
-    run()
+    app.run(host='0.0.0.0', port=6666, access_log=True)
